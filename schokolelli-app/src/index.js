fa9b93e04e69d6fcfb7ba951ea6b385e1035111b
Marcel Spahr <xrp@bluewin.ch>
2024-10-31 23:51:52 +0100

    fix: change App import to app

--- schokolelli-app/src/index.js
+++ schokolelli-app/src/index.js
@@ -1,9 +1,10 @@
-import React from "react"
-import * as ReactDomClient from "react-dom/client"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./index.css"
-import App from "./App";
+// src/index.js
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./app";
 
-const root = ReactDomClient.createRoot(document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
-root.render(<App></App>);
+root.render(<App />);
